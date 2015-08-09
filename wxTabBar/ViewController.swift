16821357b67c5e3a84137784c8df28cc5d0c3e21
xxycode <245718977@qq.com>
2015-08-09 17:36:01 +0800

    iOS上实现安卓版微信的tabbar切换的效果

--- wxTabBar/ViewController.swift
+++ wxTabBar/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIScrollViewDelegate{
+    @IBOutlet weak var containerScrollView: UIScrollView!
+    @IBOutlet weak var tabBar: UIView!
+    @IBOutlet weak var button4MarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var button3MarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var button2MarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var button1MarginLeft: NSLayoutConstraint!
+    var selectedIndex:Int = 1
     override func viewDidLoad() {
         super.viewDidLoad()
+        containerScrollView.delegate = self
+        (tabBar.viewWithTag(1) as! UIButton).selected = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +27,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func barButtonDidClicked(sender: AnyObject) {
+        var button = sender as! UIButton
+        resetButton()
+        button.selected = true
+        var screenWidth = view.frame.size.width
+        containerScrollView.setContentOffset(CGPointMake(CGFloat(button.tag - 1) * screenWidth, 0), animated: false)
+        selectedIndex = button.tag
+        
+    }
+    
+    func resetButton(){
+        for item in tabBar.subviews{
+            if item.isKindOfClass(NSClassFromString("UIButton")) == true{
+                (item as! UIButton).selected = false
+            }
+        }
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        var screenWidth = view.frame.size.width
+        containerScrollView.setContentOffset(CGPointMake(CGFloat(selectedIndex - 1) * screenWidth, 0), animated: false)
+        var marginLeft:CGFloat = (screenWidth - 46 * 4) / 8
+        button1MarginLeft.constant = marginLeft
+        button2MarginLeft.constant = 2 * marginLeft
+        button3MarginLeft.constant = 2 * marginLeft
+        button4MarginLeft.constant = 2 * marginLeft
+        super.viewDidLayoutSubviews()
+    }
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        var screenWidth = view.frame.size.width
+        var offsetX = scrollView.contentOffset.x
+        var selecedButtonIndex = Int(offsetX/screenWidth) + 1
+        println(selecedButtonIndex)
+        resetButton()
+        var button = tabBar.viewWithTag(selecedButtonIndex) as? UIButton
+        selectedIndex = selecedButtonIndex
+        button?.selected = true
+    }
 
 }
 
